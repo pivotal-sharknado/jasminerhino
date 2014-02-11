7f3d3dc433f38af3730626e34f19819db127fdc6
Grant Hutchins and kiranchang kanagasekar <pair+grant+kkanagasekar@pivotallabs.com>
2014-02-11 10:41:10 -0500

    rails new jasminerhino

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jasminerhino::Application.config.secret_token = '2649973152c9bc9811de0d2d4d2e266d07b02f02a85976fb70af9971eed2643c0b4b6c998f5c89a7637b426ff3cf81d8c661128afb6abefbf81e5829095e526b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Jasminerhino::Application.config.session_store :cookie_store, key: '_jasminerhino_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Jasminerhino::Application.config.session_store :active_record_store
